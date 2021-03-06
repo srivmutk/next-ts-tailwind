4f2f771705aa989626b5eb3f533d47c09bfc0e02
Sysnomid <sam@sysnomid.com>
2021-03-06 13:43:19 -0500

    main

--- index.ts
+++ index.ts
@@ -1,30 +1,33 @@
 #!/usr/bin/node 
-import prompts from "prompts"
-import exec from "await-exec"
+import exec from "await-exec";
 import { spawn } from "child_process";
+import colors from "colors";
+import rimraf from "rimraf";
 
-let resVal: string;
+const data = {
+	args: process.argv.slice(2),
+	baseDir: "next-ts-tailwind"
+};
 
-function resolve(){
-	console.log("Installing Dependencies...")
-	process.chdir(`${resVal}`);
-	const installer = spawn("npm", ['install'], {stdio: 'inherit'})
-	console.log(`Happy Hacking`)
-	return installer;
-}
+data.args[0];
 
 (async () => {	
-	const response = await prompts({
-    	type: 'text',
-    	name: 'value',
-    	message: 'Name of Folder?'
-	  });
-	
-	 await exec(`git clone -b boilerplate https://github.com/Sysnomid/next-ts-tailwind.git ${response.value}`);  
- 
-	resVal = response.value;
-	
-	resolve();
+	try {
+		await exec(`git clone -b boilerplate https://github.com/Sysnomid/next-ts-tailwind.git ${data.args}`);  
+		process.chdir(`${data.args}`);
+        const installer = spawn("npm", ['install'], { stdio: 'inherit' });
+        console.log(colors.blue(`Happy Hacking`));
+        console.log(colors.green("Installing Dependencies..."));
+        return installer;
+	} catch(error) {
+		if (data.args[0] === undefined){
+			console.log(colors.red("ERROR: Please enter a project name"));
+		} else {
+			console.log(colors.red(error))
+		}
+        rimraf.sync(data.baseDir);
+        return;
+	}
 })();
 
 
